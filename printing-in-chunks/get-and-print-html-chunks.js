7fa82f173dd5eb57170e05be3bfb17752c95b511
cocomatt <matt.currie@outlook.com>
2018-01-22 20:06:02 +0000

    commits step 1 - printing in chunks

--- printing-in-chunks/get-and-print-html-chunks.js
+++ printing-in-chunks/get-and-print-html-chunks.js
@@ -20,7 +20,8 @@ function getAndPrintHTMLChunks () {
 
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      console.log('Chunk Received. Length: ', data.length);
+      //console.log('Chunk Received. Length: ', data.length);
+      console.log(data, '\n');
     });
 
     // the callback is invoked when all of the data has been received
